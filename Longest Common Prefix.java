0a7b2aab0f0576cc93d83a4cc437249bc587dab2
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-04-24 14:08:54 +0530

    Longest Common Prefix

--- Longest Common Prefix.java
+++ Longest Common Prefix.java
@@ -2,31 +2,6 @@ import java.util.*;
 
 public class Solution {
     public String longestCommonPrefix(ArrayList<String> A) {
-    //   int i,n,minlen;
-    //   n=A.size();
-    //   if(n==0)
-    //   {
-    //       return "";
-    //   }
-    //   minlen=Integer.MAX_VALUE;
-    //   for(String s : A)
-    //   {
-    //       minlen=Math.min(minlen,s.length());
-    //   }
-    //   StringBuilder str = new StringBuilder();
-    //   for(i=0;i<minlen;i++)
-    //   {
-    //       char ch = A.get(0).charAt(i);
-    //       for(String s1 : A)
-    //       {
-    //           if(s1.charAt(i)!=ch)
-    //           {
-    //               return str.toString();
-    //           }
-    //       }
-    //       str.append(ch);
-    //   }
-    //   return str.toString();
     int i,n,minlen;
     n=A.size();
     if(n==0)
