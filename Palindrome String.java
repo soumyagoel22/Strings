ca35cdabbdc39df982f1082145733a33e8c3d2dc
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-04-25 14:14:15 +0530

    Create Palindrome String.java

---
+++ Palindrome String.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int isPalindrome(String A) {
+        A = A.replaceAll("[^a-zA-Z0-9]","");
+        System.out.println(A);
+        A = A.toLowerCase();
+        System.out.println(A);
+        int i,n,j;
+        n=A.length();
+        i=0;
+        j=n-1;
+        while(i<=j)
+        {
+            if(A.charAt(i)!=A.charAt(j))
+            return 0;
+            else
+            {
+                i++;
+                j--;
+                continue;
+            }
+        }
+        return 1;
+    }
+}
