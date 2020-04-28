ecc710f28dcb06f1ea968cbc80ada5e2d6bd7a09
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-04-28 16:35:42 +0530

    Create Add Binary Strings.java

---
+++ Add Binary Strings.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public String addBinary(String A, String B) {
+        // StringBuilder str=new StringBuilder();
+        // int i,j,carry,n1,n2,num;
+        // i=A.length()-1;
+        // j=B.length()-1;
+        // carry=0;
+        // while(i>=0 || j>=0 || carry>0)
+        // {
+        //      n1 = i >= 0 ? (A.charAt(i) - '0') : 0;
+        //      n2 = j >= 0 ? (B.charAt(j) - '0') : 0;
+        //      num = n1 + n2 + carry;
+        //     str.insert(0, num % 2);
+        //     carry = num / 2;
+        //     i--;
+        //     j--;
+        // }
+        // return str.toString();
+        int i,j,n1,n2,carry,num;
+        i=A.length()-1;
+        j=B.length()-1;
+        carry=0;
+        StringBuilder str = new StringBuilder();
+        while(i>=0 || j>=0 || carry>0)
+        {
+            n1= (i>=0)?(A.charAt(i)-'0'):0;
+            n2= (j>=0)?(B.charAt(i)-'0'):0;
+            num=n1+n2+carry;
+            str.insert(0,num%2);
+            carry = num/2;
+            i--;
+            j--;
+        }
+        return str.toString();
+    }
+    }
