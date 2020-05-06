fb6b1cc4ef7a4c23049106df231d0cd404baeb8b
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-05-06 11:53:29 +0530

    Multiply Strings

--- Multiply Strings.java
+++ Multiply Strings.java
@@ -1,31 +1,7 @@
 import java.util.*;
 
 public class Solution {
-    // public String multiply(String A, String B) {
         public static String multiply(String a, String b) {
-        // String n1 = new StringBuilder(a).reverse().toString();
-        // String n2 = new StringBuilder(b).reverse().toString();
-        
-        // int[] d = new int[a.length() + b.length()];
-        
-        // for(int i =0; i < n1.length(); i++){
-        //     for(int j =0; j < n2.length(); j++){
-        //         d[i+j] += (n1.charAt(i) - '0') * (n2.charAt(j) - '0');
-        //     }
-        // }
-        // StringBuilder sb = new StringBuilder();
-        // for(int i = 0; i < d.length; i++){
-        //     int mod = d[i] % 10;
-        //     int carry = d[i] / 10;
-        //     if(i + 1 < d.length) 
-        //         d[i + 1] += carry;
-        //     sb.insert(0, mod);
-        // }
-        
-        // while(sb.charAt(0) == '0' && sb.length() > 1)
-        //     sb.deleteCharAt(0);
-        
-        // return sb.toString();
         int i,j,n,m,mod,carry;
         String a1 = new StringBuilder(a).reverse().toString();
         String b1 = new StringBuilder(b).reverse().toString();
