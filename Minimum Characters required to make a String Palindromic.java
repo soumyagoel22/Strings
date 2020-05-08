8e899ef77f6f7d1909cf8f9ed6d1687103e4f3b8
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-05-08 12:03:33 +0530

    Create Minimum Characters required to make a String Palindromic.java

---
+++ Minimum Characters required to make a String Palindromic.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int solve(String A) {
+    int i,n,start,end,temp;
+    n=A.length();
+    start=0;
+    end=n-1;
+    temp=end;
+    while(start<=temp)
+    {
+        if(A.charAt(start)==A.charAt(temp))
+        {
+            start++;
+            temp--;
+        }
+        else
+        {
+            start=0;
+            end--;
+            temp=end;
+        }
+    }
+    return (n-end-1);
+    }
+}
