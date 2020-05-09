0067a3667ab5eacdde98e30fec927202b092dd7a
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-05-09 13:30:08 +0530

    Create Compare Version Numbers.java

---
+++ Compare Version Numbers.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int compareVersion(String A, String B) {
+        String[] a = A.split("\\.");
+        String[] b = B.split("\\.");
+        int i,j,n,m;
+        n=a.length;
+        m=b.length;
+        double a1,b1;
+        for(i=0,j=0;i<n && j<m;i++,j++)
+        {
+            a1 = Double.parseDouble(a[i]);
+            b1 = Double.parseDouble(b[j]);
+            if(a1>b1)
+            return 1;
+            else if(a1<b1)
+            return -1;
+        }
+        while(i<n)
+        {
+            a1 = Double.parseDouble(a[i]);
+            if(a1>0)
+            return 1;
+            i++;
+        }
+        while(j<m)
+        {
+            b1 = Double.parseDouble(b[j]);
+            if(b1>0)
+            return -1;
+            j++;
+        }
+        return 0;
+    }
+}
